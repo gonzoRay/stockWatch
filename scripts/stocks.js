ce1830437d563253f958ebbc5640ca38cb25ec71
Reagan Cooper <raygunc@gmail.com>
2016-03-20 19:45:50 -0400

    fixing preventDefault and adding error handling

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $('.search-bar button').click(function () {
+    $('.search-bar button').click(function (e) {
+        e.preventDefault();
         var userInput = $('#stockInputSymbol').val();
         lookupStock(userInput);
     });
@@ -15,15 +16,45 @@ $(document).ready(function () {
         $.get(stockUrl, function (data, status) {
             var response = data.query.results.quote;
 
-            $('.stock-info .stock-name').text(response.Symbol);
-            $('.stock-info .stock-desc').text(response.Name);
-            $('.stock-info .stock-last-traded').text('last traded: ' + response.LastTradeDate);
-
-            var currentAsk = Number(response.Ask).toFixed(0);
-            $('.stock-info .stock-price').text('$' + currentAsk);
-
-            $('.stock-info .stock-price-change').text(response.Change);
+            if(status === 'success' && response && response.Name) {
+                clearStockInfo();
+                clearError();
+                updateStockInfo(response);
+            } else if (status === 'success' && response && !response.Name){
+                clearStockInfo();
+                setError('No results found.');
+            } else {
+                clearStockInfo();
+                setError('An unexpected error has occurred.');
+            }
         });
+    }
+
+    function updateStockInfo(response) {
+        $('.stock-info .stock-name').text(response.Symbol);
+        $('.stock-info .stock-desc').text(response.Name);
+        $('.stock-info .stock-last-traded').text('last traded: ' + response.LastTradeDate);
+
+        var currentAsk = Number(response.Ask).toFixed(0);
+        $('.stock-info .stock-price').text('$' + currentAsk);
+
+        $('.stock-info .stock-price-change').text(response.Change);
+    }
+
+    function clearStockInfo() {
+        $('.stock-info .stock-name').empty();
+        $('.stock-info .stock-desc').empty();
+        $('.stock-info .stock-last-traded').empty();
+        $('.stock-info .stock-price').empty();
+        $('.stock-info .stock-price-change').empty();
+    }
+
+    function setError(errorText) {
+        $('.error-info #errorMessage').text(errorText);
+    }
+
+    function clearError() {
+        $('.error-info #errorMessage').empty();
     }
 });
 
