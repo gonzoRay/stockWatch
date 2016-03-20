3826a3a02438368eaf84b405d437dd803ac74f30
Reagan Cooper <raygunc@gmail.com>
2016-03-20 19:33:20 -0400

    dry up code, add error handling

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -16,19 +16,46 @@ $(document).ready(function () {
         $.get(stockUrl, function (data, status) {
             var response = data.query.results.quote;
 
-            if(status === 200) {
-                $('.stock-info .stock-name').text(response.Symbol);
-                $('.stock-info .stock-desc').text(response.Name);
-                $('.stock-info .stock-price').text(response.Ask);
-                $('.stock-info .stock-price-change').text(response.Change);
+            if(status === 'success' && response && response.Name) {
+                clearStockInfo();
+                clearError();
+                updateStockInfo(response);
+            } else if (status === 'success' && response && !response.Name){
+                clearStockInfo();
+                setError('No results found.');
+            } else {
+                clearStockInfo();
+                setError('An unexpected error has occurred.');
             }
         });
     }
 
-    function isPriceChangePositive(priceChange) {
-        return priceChange.indexOf('+') > -1;
+    function updateStockInfo(response) {
+        $('.stock-info .stock-name').text(response.Symbol);
+        $('.stock-info .stock-desc').text(response.Name);
+        $('.stock-info .stock-last-traded').text('last traded: ' + response.LastTradeDate);
+
+        var currentAsk = Number(response.Ask).toFixed(0);
+        $('.stock-info .stock-price').text('$' + currentAsk);
+
+        $('.stock-info .stock-price-change').text(response.Change);
     }
 
+    function clearStockInfo() {
+        $('.stock-info .stock-name').empty();
+        $('.stock-info .stock-desc').empty();
+        $('.stock-info .stock-last-traded').empty();
+        $('.stock-info .stock-price').empty();
+        $('.stock-info .stock-price-change').empty();
+    }
+
+    function setError(errorText) {
+        $('.error-info #errorMessage').text(errorText);
+    }
+
+    function clearError() {
+        $('.error-info #errorMessage').empty();
+    }
 });
 
 
